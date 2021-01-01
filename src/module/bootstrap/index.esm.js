da293f30d5f9ee9e2c92a4df13c25d7714cfde50
Ben Edwards <bene@pageuppeople.com>
2021-01-01 19:35:04 +1100

    Remove most bootstrap js components
    Saves a little compile time

--- src/module/bootstrap/index.esm.js
+++ src/module/bootstrap/index.esm.js
@@ -5,28 +5,28 @@
  * --------------------------------------------------------------------------
  */
 
-import Alert from './alert'
-import Button from './button'
-import Carousel from './carousel'
+// import Alert from './alert'
+// import Button from './button'
+// import Carousel from './carousel'
 import Collapse from './collapse'
 import Dropdown from './dropdown'
-import Modal from './modal'
-import Popover from './popover'
-import ScrollSpy from './scrollspy'
-import Tab from './tab'
-import Toast from './toast'
+// import Modal from './modal'
+// import Popover from './popover'
+// import ScrollSpy from './scrollspy'
+// import Tab from './tab'
+// import Toast from './toast'
 import Tooltip from './tooltip'
 
 export {
-  Alert,
-  Button,
-  Carousel,
+  // Alert,
+  // Button,
+  // Carousel,
   Collapse,
   Dropdown,
-  Modal,
-  Popover,
-  ScrollSpy,
-  Tab,
-  Toast,
+  // Modal,
+  // Popover,
+  // ScrollSpy,
+  // Tab,
+  // Toast,
   Tooltip
 }
