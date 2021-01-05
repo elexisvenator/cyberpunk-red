42dbd30a9bc5cd8a0f1b438d2be5f8ac55f76b39
WhiteMagic <white.magic@gmx.de>
2021-01-05 23:11:28 +0100

    Class hierarchy for rollable actions
    - Base class unifies presentation and provides abstract function to
    customize behaviour
    - Derived classes implement roll() to perform specific roll logic

---
+++ src/module/rollable.ts
@@ -0,0 +1,56 @@
+/**
+ * Abstract class for all roll implementations.
+ * 
+ * An instance is constructed with the information necessary to perform a
+ * roll and the base class provides a method which handles rolling and
+ * rendering the outcome for all derived implementations.
+ */
+export abstract class Rollable {
+  formula: string;
+  actor: Actor;
+  item?: Item;
+
+  /**
+   * Creates a new rollable instance.
+   * 
+   * @param formula Formula to use during rolling
+   * @param actor Actor whose statistics are to be used for rolling
+   * @param item Item whose statistics are to be used for rolling
+   */
+  constructor(formula: string, actor: Actor, item?: Item) {
+    this.formula = formula;
+    this.actor = actor;
+    this.item = item;
+  }
+
+  /**
+   * Implements the specific rolling logic using the data provided to the constructor.
+   */
+  abstract roll(): void;
+  
+  protected _executeRoll(roll: Roll) {
+    const actor = this.actor;
+    roll
+      .roll()
+      .render()
+      .then((content) => {
+        ChatMessage.create({
+          user: game.user._id,
+          speaker: ChatMessage.getSpeaker({actor}),
+          content,
+          sound: CONFIG.sounds.dice,
+        });
+      });
+  }
+}
+
+/**
+ * Formula only based rollable implementation.
+ * 
+ * Performs a roll according to the provided formula only.
+ */
+export class FormulaRollable extends Rollable {
+  roll(): void {
+    this._executeRoll(new Roll(this.formula));
+  }
+}
