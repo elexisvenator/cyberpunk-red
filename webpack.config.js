095d3e7e4ccc5db7c7d6ee8ae5efde19009d8c32
Ben Edwards <bene@pageuppeople.com>
2021-01-07 18:19:38 +1100

    fix broken webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,44 @@
-import { join } from 'path';
-import { readJSONSync } from 'fs-extra';
+const path = require('path');
+const fs = require('fs-extra');
 
-const babelConfig = readJSONSync(join(process.cwd(), '.babelrc'));
+const babelConfig = fs.readJSONSync(path.join(process.cwd(), '.babelrc'));
 
-export const entry = {
-	cpred: './src/cpred.ts',
-};
-export const output = {
-	path: join(process.cwd(), 'dist'),
-	publicPath: '/',
-	filename: '[name].js',
-	library: 'cpred',
-	libraryTarget: 'window',
-};
-export const module = {
-	rules: [
-		{
-			test: /\.ts$/,
-			exclude: /node_modules/,
-			use: [
-				{
-					loader: 'babel-loader',
-					options: {
-						...babelConfig,
+module.exports = {
+	entry: {
+		cpred: './src/cpred.ts',
+	},
+	output: {
+		path: path.join(process.cwd(), 'dist'),
+		publicPath: '/',
+		filename: '[name].js',
+		library: 'cpred',
+		libraryTarget: 'window',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							...babelConfig,
+						},
 					},
-				},
-				{
-					loader: 'ts-loader',
-					options: {
-						appendTsSuffixTo: [/\.vue$/],
+					{
+						loader: 'ts-loader',
+						options: {
+							appendTsSuffixTo: [/\.vue$/],
+						},
 					},
-				},
-			],
-		},
-	],
-};
-export const plugins = [];
-export const resolve = {
-	extensions: ['.js', '.ts', '.json'],
-	modules: ['node_modules'],
+				],
+			},
+		],
+	},
+	plugins: [],
+	resolve: {
+		extensions: ['.js', '.ts', '.json'],
+		modules: ['node_modules'],
+	},
 };
