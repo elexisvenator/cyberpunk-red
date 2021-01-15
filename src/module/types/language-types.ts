d6f0f5dfbe3a5e23fc55782210433c744766c0ef
WhiteMagic <white.magic@gmx.de>
2021-01-15 17:37:01 +0100

    Language types regenerated

--- src/module/types/language-types.ts
+++ src/module/types/language-types.ts
@@ -3,226 +3,232 @@
 // This file should be generated every time you add to en.json
 
 export interface Root {
-  cpred: Cpred;
+  cpred: Cpred
 }
 
 export interface Cpred {
-  sheet: Sheet;
-  tab: Tab;
-  skillGroups: SkillGroups;
-  skills: Skills;
-  subSkills: SubSkills;
+  sheet: Sheet
+  tab: Tab
+  skillGroups: SkillGroups
+  skills: Skills
+  subSkills: SubSkills
 }
 
 export interface Sheet {
-  bullets: string;
-  magazine: string;
-  character: string;
-  cyberware: string;
-  npc: string;
-  ice: string;
-  weapon: string;
-  name: string;
-  current: string;
-  max: string;
-  rez: string;
-  damage: string;
-  price: string;
-  cost: string;
-  description: string;
-  effect: string;
-  weapon_type: string;
-  combat_skill: string;
-  reload: string;
-  stats: Stats;
-  iceactions: Iceactions;
-  selecticeclass: string;
-  iceclasses: Iceclasses;
-  weapon_actions: WeaponActions;
-  select_weapon_type: string;
-  select_weapon_skill: string;
-  weapon_types: WeaponTypes;
-  weapon_stats: WeaponStats;
+  bullets: string
+  magazine: string
+  character: string
+  cyberware: string
+  npc: string
+  ice: string
+  weapon: string
+  name: string
+  current: string
+  max: string
+  rez: string
+  damage: string
+  price: string
+  cost: string
+  description: string
+  effect: string
+  weapon_type: string
+  combat_skill: string
+  reload: string
+  health: string
+  luck: string
+  armor_body: string
+  armor_head: string
+  stats: Stats
+  iceactions: Iceactions
+  selecticeclass: string
+  iceclasses: Iceclasses
+  weapon_actions: WeaponActions
+  select_weapon_type: string
+  select_weapon_skill: string
+  weapon_types: WeaponTypes
+  weapon_stats: WeaponStats
+  weapon_tags: WeaponTags
+  cyberware_stats: CyberwareStats
 }
 
 export interface Stats {
-  perception: string;
-  speed: string;
-  attack: string;
-  defence: string;
-  int: string;
-  ref: string;
-  dex: string;
-  tech: string;
-  cool: string;
-  will: string;
-  luck: string;
-  move: string;
-  body: string;
-  emp: string;
+  perception: string
+  speed: string
+  attack: string
+  defence: string
+  int: string
+  ref: string
+  dex: string
+  tech: string
+  cool: string
+  will: string
+  luck: string
+  move: string
+  body: string
+  emp: string
 }
 
 export interface Iceactions {
-  blockslide: string;
-  ambush: string;
-  attack: string;
-  defend: string;
+  blockslide: string
+  ambush: string
+  attack: string
+  defend: string
 }
 
 export interface Iceclasses {
-  antipersonnel: string;
-  antiprogram: string;
+  antipersonnel: string
+  antiprogram: string
 }
 
 export interface WeaponActions {
-  aimed_shot: string;
-  single_shot: string;
-  autofire: string;
-  suppressive_fire: string;
-  shotgun_shell: string;
-  explosive: string;
-  damage: string;
+  aimed_shot: string
+  single_shot: string
+  autofire: string
+  suppressive_fire: string
+  shotgun_shell: string
+  explosive: string
+  damage: string
 }
 
 export interface WeaponTypes {
-  pistol: string;
-  smg: string;
-  shotgun: string;
-  assault_rifle: string;
-  sniper_rifle: string;
-  bow_crossbow: string;
-  grenade_launcher: string;
-  rocket_launcher: string;
-  melee_weapon: string;
+  pistol: string
+  smg: string
+  shotgun: string
+  assault_rifle: string
+  sniper_rifle: string
+  bow_crossbow: string
+  grenade_launcher: string
+  rocket_launcher: string
+  melee_weapon: string
 }
 
 export interface WeaponStats {
-  damage: string;
-  rof: string;
-  type: string;
-  skill: string;
-  hands_required: string;
-  reload_turns: string;
-  autofire_max: string;
-  magazine: string;
-  is_ranged: string;
-  is_concealable: string;
+  damage: string
+  rof: string
+  type: string
+  skill: string
+  hands_required: string
+  reload_turns: string
+  autofire_max: string
+  magazine: string
+  is_ranged: string
+  is_concealable: string
 }
 
 export interface WeaponTags {
-  "single_shot": string;
-  "aimed_shot": string;
-  "autofire": string;
-  "suppressive_fire": string;
-  "shotgun_shell": string;
-  "explosive": string;
-  "throwable": string;
-  "bayonet": string;
-  "drum_magazine": string;
-  "extended_magazine": string;
-  "underbarrel_grenade_launcher": string;
-  "infrared_nightvision_scope": string;
-  "underbarrel_shotgun": string;
-  "smargun_link": string;
-  "sniping_scope": string;
+  single_shot: string
+  aimed_shot: string
+  autofire: string
+  suppressive_fire: string
+  shotgun_shell: string
+  explosive: string
+  throwable: string
+  bayonet: string
+  drum_magazine: string
+  extended_magazine: string
+  underbarrel_grenade_launcher: string
+  infrared_nightvision_scope: string
+  underbarrel_shotgun: string
+  smargun_link: string
+  sniping_scope: string
 }
 
 export interface CyberwareStats {
-  type: string;
-  foundation: string;
-  install: string;
-  slots: number;
-  humanity_loss: number;
-  humanity_loss_roll: string;
+  foundation: string
+  type: string
+  install: string
+  slots: string
+  humanity_loss: string
+  humanity_loss_roll: string
 }
 
 export interface Tab {
-  description: string;
-  combat: string;
+  description: string
+  combat: string
 }
 
 export interface SkillGroups {
-  awareness: string;
-  body: string;
-  control: string;
-  education: string;
-  fighting: string;
-  performance: string;
-  ranged: string;
-  social: string;
-  technique: string;
+  awareness: string
+  body: string
+  control: string
+  education: string
+  fighting: string
+  performance: string
+  ranged: string
+  social: string
+  technique: string
 }
 
 export interface Skills {
-  accounting: string;
-  acting: string;
-  air_vehicle_tech: string;
-  animal_handling: string;
-  archery: string;
-  athletics: string;
-  autofire: string;
-  basic_tech: string;
-  brawling: string;
-  bribery: string;
-  bureaucracy: string;
-  business: string;
-  composition: string;
-  conceal_object: string;
-  concentration: string;
-  contortionist: string;
-  conversation: string;
-  criminology: string;
-  cryptography: string;
-  cybertech: string;
-  dance: string;
-  deduction: string;
-  demolitions: string;
-  drive_air: string;
-  drive_land: string;
-  drive_sea: string;
-  education: string;
-  electronics_security_tech: string;
-  endurance: string;
-  evasion: string;
-  first_aid: string;
-  forgery: string;
-  gamble: string;
-  handgun: string;
-  heavy_weapons: string;
-  human_perception: string;
-  interrogation: string;
-  land_vehicle_tech: string;
-  language: string;
-  library_search: string;
-  lip_reading: string;
-  local_expert: string;
-  martial_arts: string;
-  melee_weapon: string;
-  paint_draw_sculpt: string;
-  paramedic: string;
-  perception: string;
-  personal_grooming: string;
-  persuasion: string;
-  photography_film: string;
-  pick_lock: string;
-  pick_pocket: string;
-  play_instrument: string;
-  resist_torture: string;
-  riding: string;
-  science: string;
-  sea_vehicle_tech: string;
-  shoulder_arms: string;
-  stealth: string;
-  streetwise: string;
-  tactics: string;
-  tracking: string;
-  trading: string;
-  wardrobe: string;
-  weaponstech: string;
-  wilderness: string;
+  accounting: string
+  acting: string
+  air_vehicle_tech: string
+  animal_handling: string
+  archery: string
+  athletics: string
+  autofire: string
+  basic_tech: string
+  brawling: string
+  bribery: string
+  bureaucracy: string
+  business: string
+  composition: string
+  conceal_object: string
+  concentration: string
+  contortionist: string
+  conversation: string
+  criminology: string
+  cryptography: string
+  cybertech: string
+  dance: string
+  deduction: string
+  demolitions: string
+  drive_air: string
+  drive_land: string
+  drive_sea: string
+  education: string
+  electronics_security_tech: string
+  endurance: string
+  evasion: string
+  first_aid: string
+  forgery: string
+  gamble: string
+  handgun: string
+  heavy_weapons: string
+  human_perception: string
+  interrogation: string
+  land_vehicle_tech: string
+  language: string
+  library_search: string
+  lip_reading: string
+  local_expert: string
+  martial_arts: string
+  melee_weapon: string
+  paint_draw_sculpt: string
+  paramedic: string
+  perception: string
+  personal_grooming: string
+  persuasion: string
+  photography_film: string
+  pick_lock: string
+  pick_pocket: string
+  play_instrument: string
+  resist_torture: string
+  riding: string
+  science: string
+  sea_vehicle_tech: string
+  shoulder_arms: string
+  stealth: string
+  streetwise: string
+  tactics: string
+  tracking: string
+  trading: string
+  wardrobe: string
+  weaponstech: string
+  wilderness: string
 }
 
 export interface SubSkills {
-  streetslang: string;
-  your_home: string;
+  streetslang: string
+  your_home: string
 }
