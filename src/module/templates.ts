165ca366fc886d1cca05674b20c2cea2bec0f074
WhiteMagic <white.magic@gmx.de>
2021-01-15 10:07:45 +0100

    Select and checkbox form element partials added

--- src/module/templates.ts
+++ src/module/templates.ts
@@ -2,7 +2,9 @@ export const preloadTemplates = async function (): Promise<void> {
   const templatePaths: string[] = [
     // Add paths to "systems/cpred/templates"
     "partials/actor-resource.html",
+    "partials/attribute-checkbox.html",
     "partials/attribute-input.html",
+    "partials/attribute-select.html",
     "partials/character-combat-tab.html",
     "partials/character-description-tab.html",
     "partials/character-gear-tab.html",
